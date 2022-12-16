4ab7f036d4290ce29f5487c8bc5b267b849695d0
Onur Alp Bicer <onuralpbicer@gmail.com>
2022-12-16 21:10:45 +1100

    highlight ability for the search filter

--- src/app/core/components/contact-list/contact-list.component.ts
+++ src/app/core/components/contact-list/contact-list.component.ts
@@ -1,11 +1,12 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import { Contact } from '@app/core/models';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class ContactListComponent implements OnInit {
 
@@ -13,6 +14,7 @@ export class ContactListComponent implements OnInit {
   @Input() contacts: Contact[];
   @Input() curPage: number;
   @Input() totalPages: number;
+  @Input() filter: string;
   @Output() edit = new EventEmitter<Contact>();
   @Output() show = new EventEmitter<Contact>();
   @Output() remove = new EventEmitter<Contact>();
@@ -23,6 +25,18 @@ export class ContactListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  filteredContent(content: string) {
+    const startIndex = content.toLowerCase().indexOf(this.filter)
+    
+    if (startIndex === -1) return content
+
+    return content.slice(0, startIndex) + 
+          "<span class=\"highlight\">" + 
+          content.slice(startIndex, startIndex + this.filter.length) + 
+          "</span>" + 
+          content.slice(startIndex + this.filter.length)
+  }
 
   changePage(page: number) {
     this.goToPage.emit(page)
